f8e5b91746c33fe85d974829ee2f0254f44c2692
petr0n <petron@gmail.com>
2020-07-20 21:13:29 -0500

    add keeping modal state

--- public/js/dc_modal.js
+++ public/js/dc_modal.js
@@ -8,19 +8,20 @@ var $ = require('jquery');
  * Initialize modal
  */
 function dc_init() {
-	btn = $('body').find('#dc_image-search-box g.floor');
+	const btn = $('body').find('#dc_image-search-box g.floor');
 	modal = $('.dc_modal');
+	isFloorplan();
 	btn.each(function(x){
-		var this_but = $(this);
-		var file_name = $(this).data('modal');
-		var title = $(this).data('display-name');
-		var path_to_file = '/wp-content/plugins/DoranCafe/public/partials/';
+		const this_but = $(this);
+		const file_name = $(this).data('modal');
+		const title = $(this).data('display-name');
+		const path_to_file = '/wp-content/plugins/DoranCafe/public/partials/';
 		this_but.on('click', function(e){
 			e.preventDefault();	
 			modal.fadeIn('fast', function(){
-				// console.log(title);
 				modal.find('h3').text(title);
 				$('.dc_modal-form-wrapper').load( path_to_file + file_name + '.php');
+				saveLocation(file_name, title);
 			});
 		});
 	});
@@ -32,6 +33,7 @@ function dc_close_modal() {
 	$('#dc_upload_modal .dc_modal_close').on('click', function(e){
 		if (modal.is(':visible')) {
 			modal.fadeOut('fast', function() {
+				localStorage.removeItem('dc_floor');
 				$('.dc_modal-form-wrapper').empty();
 			});
 		}	
@@ -39,12 +41,52 @@ function dc_close_modal() {
 	window.onclick = function(e) {
 		if ($(e.target).hasClass('dc_modal')) {
 			modal.fadeOut('fast', function() {
+				localStorage.removeItem('dc_floor');
 				$('.dc_modal-form-wrapper').empty();
 			});
 		}
 	}
 }
 
+
+
+function isFloorplan() {
+	if (localStorage.getItem('dc_floor')) {
+		const path_to_file = '/wp-content/plugins/DoranCafe/public/partials/';
+		const url = path_to_file + localStorage.getItem('dc_floor') + '.php';
+		// console.log('url', url);
+		modal.fadeIn('fast', function(){
+			modal.find('h3').text(localStorage.getItem('dc_modal_title'));
+			$('.dc_modal-form-wrapper').load(url);
+		});
+
+	}
+	// reset storage
+	// $('.av-inner-tab-title').on('click', function() {
+	// 	window.onhashchange = function() {
+	// 		if (window.location.hash === '#search-by-interactive-map' || window.location.hash === '#search-by-feature') {
+	// 			if (localStorage.getItem('dc_floor')) {
+	// 				localStorage.removeItem('dc_floor');
+	// 				localStorage.removeItem('dc_modal_title');
+	// 				if (modal.is(':visible')) {
+	// 					modal.fadeOut('fast', function() {
+	// 						$('.dc_modal-form-wrapper').empty();
+	// 					});
+	// 				}	
+	// 			}
+	// 		}
+	// 	}
+	// });
+}
+
+function saveLocation(dc_floor, dc_title){
+	if (typeof(Storage) !== "undefined") {
+		localStorage.setItem('dc_floor', dc_floor);
+		localStorage.setItem('dc_modal_title', dc_title);
+	} else {
+		console.log('local storage not available')
+	}
+}
 
 
 /**
